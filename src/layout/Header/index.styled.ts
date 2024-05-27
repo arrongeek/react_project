f0d6c9b26ca89e338768b53c4b46b08d6992c3ca
seopit95 <dskim2104@gmail.com>
2024-05-28 00:26:40 +0900

    Header Component developing

---
+++ src/layout/Header/index.styled.ts
@@ -0,0 +1,17 @@
+import styled, { css } from 'styled-components';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 0 20px 20px 20px;
+`;
+
+const LogoImg = styled.img`
+  width: 200px;
+  height: 120px;
+`;
+
+export const HeaderStyle = {
+    HeaderContainer,
+    LogoImg
+};
--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,8 +1,16 @@
+import { HeaderStyle as S } from "./index.styled";
+
 const Header = () => {
     return (
-        <header>
-            <h1>header</h1>
-        </header>
+        <S.HeaderContainer>
+            <S.LogoImg src="https://img.hankyung.com/photo/202401/AKR20240119042300030_01_i_P4.jpg" alt="" />
+            <div>
+                검색창
+            </div>
+            <div>
+                로그인 및 회원가입
+            </div>
+        </S.HeaderContainer>
     )
 }
 
